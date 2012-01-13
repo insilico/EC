39c18dad24c5bfe661617fa63db39e7c1106f0c4
Bill White <bill.c.white@gmail.com>
2012-01-12 18:42:50 -0600

    Shell scripts for running GWAS benchmarks on various machines.

---
+++ benchmarks/run_gwas_benchmark_thor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ec --snp-data ~/analysis/bd/after_review2/chrX_and_rs17480050_removed_no-LD.bed \
+--ec-iter-remove-percent 50 \
+--ec-num-target 1000 \
+--verbose 1 \
+--out-files-prefix wtccc-all-benchmark_thor
+
